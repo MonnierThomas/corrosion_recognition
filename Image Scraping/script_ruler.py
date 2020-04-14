fd7ea06cbe60c174be164c7f5b5fdeaacfc90ce8
MonnierThomas <54896884+MonnierThomas@users.noreply.github.com>
2020-04-14 13:51:57 +0200

    Downloading image

--- Image Scraping/script_ruler.py
+++ Image Scraping/script_ruler.py
@@ -10,7 +10,7 @@ import os
 import requests
 import shutil
 
-searchterm = 'Okocha'#input your search item here
+searchterm = 'métal corrodé'#input your search item here
 url = "https://www.google.co.in/search?q="+searchterm+"&source=lnms&tbm=isch"
 browser = webdriver.Chrome()#insert path to chromedriver inside parentheses
 browser.get(url)
@@ -27,17 +27,18 @@ imges = []
 for i in html:
     if i.startswith('http') and i.split('"')[0].split('.')[-1] in extensions:
         imges.append(i.split('"')[0])
-print(imges)
-print(len(imges))
 
-for image_url in imges:
+def download_image(image_url, i):
     # Open the url image, set stream to True, this will return the stream content.
     resp = requests.get(image_url, stream=True)
     # Open a local file with wb ( write binary ) permission.
-    local_file = open('local_image.jpg', 'wb')
+    local_file = open(f'{i}.jpg', 'wb')
     # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
     resp.raw.decode_content = True
     # Copy the response stream raw data to local image file.
     shutil.copyfileobj(resp.raw, local_file)
     # Remove the image url response object.
     del resp
+
+for i, image_url in enumerate(imges):
+    download_image(image_url, i)
