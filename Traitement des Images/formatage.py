e1a9c4bc084fa18fc6d088d2ab6026e3cc37bb4f
cdemailly <56259357+cdemailly@users.noreply.github.com>
2020-04-30 14:32:24 +0200

    Add files via upload

--- Traitement des Images/formatage.py
+++ Traitement des Images/formatage.py
@@ -1,11 +1,44 @@
-from PIL import Image
-import numpy as np
-imgpil = Image.open("chat.jpg")  
-#jsp comment aller lui faire chercher les images au bon endroit
 
-def dimensionnement(image, W, H, Q):
+
+from os import listdir
+from os.path import isfile, join
+
+
+
+from PIL import Image
+import pandas as pd
+
+def dimensionnement(image, W, H, nv_nom):
     image = image.resize((W, H),Image.ANTIALIAS)
-    image.save(path+"{image}"+"new"+".jpg", quality=Q)
-    #en vrai faut rajouter le path où tu veux l'enregistrer avant son nom
+    image.save(nv_nom, quality=95)
+
+
+
+def formatage(path1, path2):
+    """path1 est le chemin vers le dossier regroupant les photos originales, path2 le chemin d'arrivée"""
     
+    #on commence par chercher les tailles finales de nos images
+    fichiers = [f for f in listdir(path1)]
+    W = []
+    H = []
+    
+    for photo in fichiers:
+        img = Image.open(f"{photo}")
+        (w, h) = img.size
+        W.append(w)
+        H.append(h)
+        
+    W = pd.Series(W)
+    H = pd.Series(H)
+    w_min = W.min()
+    h_min = H.min()
+    
+    i = 0
+    for photo in fichiers:
+        nv_nom = path2 + f"{i}"
+        dimensionnement(photo, w_min, h_min, nv_nom)
+
+
+        
+
 
