6ae1c017cc593f32ed817c114b4d567552a0058b
MonnierThomas <54896884+MonnierThomas@users.noreply.github.com>
2020-07-07 17:58:13 +0200

    Téléchargement des images par mots-clés

--- Image Scraping/script_ruler.py
+++ Image Scraping/script_ruler.py
@@ -1,24 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 14 11:49:14 2020
-
-@author: Thomas MONNIER
-"""
-
 from selenium import webdriver
 import os
 import requests
 import shutil
 
-searchterms = ['métal corrodé']#input your search item here
-for searchterm in searchterms:
-    url = "https://www.google.co.in/search?q="+searchterm+"&source=lnms&tbm=isch"
-    browser = webdriver.Chrome()#insert path to chromedriver inside parentheses
-    browser.get(url)
-    img_count = 0
-    extensions = { "jpg", "jpeg", "png", "gif" }
-    if not os.path.exists(searchterm):
-        os.mkdir(searchterm)
+def get_url(searchterms):
+    '''Obtient l'URL des images de Google Images pour chaque mot-clé compris dans la liste des mots-clés
+    
+    Input : searchterms : liste de chaines de caractère correspondant aux mots clés
+    
+    Output : imges : liste de chaines de caractère correspondant aux URL des images
+    '''
+    
+    for searchterm in searchterms:
+        url = "https://www.google.co.in/search?q="+searchterm+"&source=lnms&tbm=isch"
+        browser = webdriver.Chrome()#insert path to chromedriver inside parentheses
+        browser.get(url)
+        img_count = 0
+        extensions = { "jpg", "jpeg", "png", "gif" }
+        if not os.path.exists(searchterm):
+            os.mkdir(searchterm)
     
     for _ in range(500):
         browser.execute_script("window.scrollBy(0,10000)")
@@ -28,18 +28,34 @@ for searchterm in searchterms:
     for i in html:
         if i.startswith('http') and i.split('"')[0].split('.')[-1] in extensions:
             imges.append(i.split('"')[0])
+    return imges
     
-    def download_image(image_url, i):
-        # Open the url image, set stream to True, this will return the stream content.
-        resp = requests.get(image_url, stream=True)
-        # Open a local file with wb ( write binary ) permission.
-        local_file = open(f'{i}.jpg', 'wb')
-        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
-        resp.raw.decode_content = True
-        # Copy the response stream raw data to local image file.
-        shutil.copyfileobj(resp.raw, local_file)
-        # Remove the image url response object.
-        del resp
     
+    
+def download_image(image_url, i):
+    ''' Télécharge les images
+    
+    Input : image_url : chaine de caractère
+            i : int
+    '''
+    # Ouvre l'URL de l'image, définis le flux sur Vrai et retourne le contenu du flux
+    resp = requests.get(image_url, stream=True)
+    # Ouvre un fichier local grâce à 'wb'
+    local_file = open(f'{i}.jpg', 'wb')
+    # Définis decode_content comme Vrai, sinon la taille de l'image téléchargée serait de 0
+    resp.raw.decode_content = True
+    # Copie les données brutes du flux de réponses dans un fichier image local
+    shutil.copyfileobj(resp.raw, local_file)
+    # Supprimez l'objet de réponse d'URL d'image
+    del resp
+    
+    
+    
+def main():
+    searchterms = [] # Liste des mots-clés souhaités
+    imges = get_url(searchterms)
     for i, image_url in enumerate(imges):
         download_image(image_url, i)
+        
+        
+main()
